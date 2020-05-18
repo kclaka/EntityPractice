068185e63eac3124d9b87eb1ddddbe72253684e5
kclaka <kennyigbechi@gmail.com>
2020-05-18 10:41:48 -0700

    Add project files.

---
+++ EntityPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityPractice
+{
+    class Patient
+    {
+        public Patient()
+        {
+            Visits = new List<Visit>();
+        }
+
+        public int Id { get; set; }
+        public string name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public AnimalType AnimalType { get; set; }
+        public DateTime FirstVisit { get; set; }
+        public List<Visit> Visits { get; set; }
+    }
+
+    class Visit
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string ReasonForVisit { get; set; }
+        public String Outcome { get; set; }
+        public Decimal Weight { get; set; }
+        public int ParentId { get; set; }
+    }
+
+    class AnimalType
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    }
+
+}
