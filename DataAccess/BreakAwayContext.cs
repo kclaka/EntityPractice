79ac053b919c6c26929bb07a07928c8d7c67480e
kclaka <kennyigbechi@gmail.com>
2020-05-19 00:09:33 -0700

    Complete Destination Database

---
+++ DataAccess/BreakAwayContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using Model;
+
+namespace DataAccess
+{
+    public class BreakAwayContext : DbContext
+    {
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Lodging> Lodgings { get; set; }
+    }
+}
